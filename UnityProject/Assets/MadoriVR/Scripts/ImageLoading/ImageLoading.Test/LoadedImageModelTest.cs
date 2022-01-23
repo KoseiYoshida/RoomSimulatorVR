b0b3627f6b32c2574819c33d8d140429b009c569
KoseiYoshida <koseiasengineer@gmail.com>
2022-01-23 19:00:42 +0900

    Test追加

---
+++ UnityProject/Assets/MadoriVR/Scripts/ImageLoading/ImageLoading.Test/LoadedImageModelTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using NUnit.Framework;
+
+namespace MadoriVR.Scripts.ImageLoading.ImageLoading.Test
+{
+    public sealed class LoadedImageModelTest
+    {
+        [Test]
+        public void SetPass_InputInvalidPath_ThrowException()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
+            var validator = new ImagePathValidator(fileSystem);
+            var model = new LoadedImageModel(validator);
+            var notExistedPath = @"c:notExist.txt";
+            
+            Assert.That(() => model.SetPath(notExistedPath), Throws.TypeOf<ArgumentException>());
+        }
+    }
+}
