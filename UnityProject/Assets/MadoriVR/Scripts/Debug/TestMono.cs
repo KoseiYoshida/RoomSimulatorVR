d73242501e9c8f1b464e0dc5d9fecd81455ec01e
KoseiYoshida <koseiasengineer@gmail.com>
2022-01-25 09:57:57 +0900

    デバッグ用スクリプト

---
+++ UnityProject/Assets/MadoriVR/Scripts/Debug/TestMono.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MadoriVR.Scripts.LineDrawing;
+using NaughtyAttributes;
+using UnityEngine;
+using VContainer.Unity;
+
+namespace MadoriVR
+{
+    public sealed class TestMono : MonoBehaviour
+    {
+        [SerializeField] private LifetimeScope lineDrawLifetimeScope = default;
+        private DrawnLineModel _drawnLineModel;
+    
+        private void Start()
+        {
+            _drawnLineModel = (DrawnLineModel)lineDrawLifetimeScope.Container.Resolve(typeof(DrawnLineModel));
+        }
+
+        [Button("Check")]
+        private void Check()
+        {
+            foreach (var line in _drawnLineModel.Lines)
+            {
+                Debug.Log($"{line.point1} - {line.point2}");
+            }
+        }
+    }
+}
